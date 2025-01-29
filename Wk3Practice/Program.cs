fa84a50f3988959e1324fd882f6a9586b2d890fb
Jose Villa <villa1997.jv@gmail.com>
2025-01-29 13:43:19 -0600

    Add project files.

---
+++ Wk3Practice/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk3Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for loop
+            for (int i = 0; i < 100; i = i + 2) 
+            {
+                Console.WriteLine(i);
+            }
+
+            //while loop
+            // loop to keep asking user a input a string
+            // check if user input QUIT, set the condition to false
+            // else, convert it to number and proccess print all odd number less than
+
+            //declerations
+
+            //input
+            string userInput = "";
+            int number = 0;
+            bool continueToRun = true;
+
+             while (continueToRun) {
+
+                Console.WriteLine("Input a number or type QUIT to exit");\
+                userInput = Console.ReadLine(); // take in the input
+                //ToLower() converters all th echaracters to lower case
+                //Startwith check if the string with a certain character/string
+
+                if (userInput.ToLower().StartsWith("q"))
+                {
+                    //check if user input QUIT, set the continue to false
+                    continueToRun = false;
+                }
+                else 
+                {
+                    //convert it to number and proccess
+                    number = Convert.ToInt32(userInput);
+
+                    //print all od numbers less than
+                    for (int i = 1; i < number; i += 2)
+                    {
+                        Console.WriteLine(i); 
+                    }
+                }
+
+
+
+
+        }
+    }
+}
---
+++ Wk3Practice/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Wk3Practice")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Wk3Practice")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ac470390-4bf0-424c-b750-f11acfbcd014")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
