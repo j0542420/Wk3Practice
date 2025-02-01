557f9b6eadb4946535d4506048c78fc1cba47d43
Jose Villa <j0542420@amarillocollege.com>
2025-01-31 21:01:50 -0600

    finished

--- Wk3Practice/Program.cs
+++ Wk3Practice/Program.cs
@@ -11,50 +11,47 @@ namespace Wk3Practice
         static void Main(string[] args)
         {
             //for loop
-            for (int i = 0; i < 100; i = i + 2) 
+            for (int i = 0; i < 100; i = i + 2)
             {
                 Console.WriteLine(i);
             }
 
-            //while loop
-            // loop to keep asking user a input a string
-            // check if user input QUIT, set the condition to false
-            // else, convert it to number and proccess print all odd number less than
-
-            //declerations
-
-            //input
+            // Declarations
             string userInput = "";
             int number = 0;
             bool continueToRun = true;
 
-             while (continueToRun) {
+            // While loop to keep asking the user for input
+            while (continueToRun)
+            {
+                Console.WriteLine("Input a number or type QUIT to exit:");
+                userInput = Console.ReadLine(); // Take in the input
 
-                Console.WriteLine("Input a number or type QUIT to exit");\
-                userInput = Console.ReadLine(); // take in the input
-                //ToLower() converters all th echaracters to lower case
-                //Startwith check if the string with a certain character/string
-
+                // ToLower() converts all characters to lower case
+                // StartsWith() checks if the string starts with a certain character/string
                 if (userInput.ToLower().StartsWith("q"))
                 {
-                    //check if user input QUIT, set the continue to false
+                    // Check if user input is "QUIT" (or starts with 'q') and stop the loop
                     continueToRun = false;
                 }
-                else 
+                else
                 {
-                    //convert it to number and proccess
-                    number = Convert.ToInt32(userInput);
-
-                    //print all od numbers less than
-                    for (int i = 1; i < number; i += 2)
+                    // Try to convert the input to a number and process
+                    if (int.TryParse(userInput, out number))
                     {
-                        Console.WriteLine(i); 
+                        // Print all odd numbers less than the given number
+                        Console.WriteLine($"Odd numbers less than {number}:");
+                        for (int i = 1; i < number; i += 2)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid number or type QUIT to exit.");
                     }
                 }
-
-
-
-
+            }
         }
     }
 }
